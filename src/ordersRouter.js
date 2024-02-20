a9e208c67da7694bf2428efa7aadde27e7d1de55
Ithink322 <experementsgamesbyforest@gmail.com>
2024-02-20 16:00:06 +0300

    add the path to collect all orders

--- src/ordersRouter.js
+++ src/ordersRouter.js
@@ -25,5 +25,23 @@ router.post("/create", async (req, res) => {
     res.status(400).json({ message: "Error creating order" });
   }
 });
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
+
+router.get("/collect", async (req, res) => {
+  try {
+    const userId = req.query.userId;
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      console.log("Invalid userId:", userId);
+      return res.status(400).json({ message: "Invalid userId" });
+    }
+    const userIdObjectId = new ObjectId(userId);
+    const orders = await Order.find({ userId: userIdObjectId });
+    return res.json(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching orders" });
+  }
+});
 
 module.exports = router;
