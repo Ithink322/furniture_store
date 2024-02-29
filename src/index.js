4c09935542c196c9ce7dfe9a3ec39eb5f5b7fe94
Ithink322 <experementsgamesbyforest@gmail.com>
2024-02-29 07:46:37 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json({ limit: "500mb" }));
-app.use(express.urlencoded({ limit: "500mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/auth", authRouter);
 app.use("/questions", questionsRouter);
 app.use("/reviews", reviewsRouter);
